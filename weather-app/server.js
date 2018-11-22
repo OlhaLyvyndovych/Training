17b8c6aede6fd5b6a4ccdfd37227b355a0b50434
Olha Lyvyndovych <olha.lyvyndovych@gmail.com>
2018-11-22 13:02:45 +0200

    initial commit

---
+++ weather-app/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+
+const apiKey = '33e1bb96ff4393e2257568fbaa8eab5c';
+
+
+app.use(express.static('public')); // allows access to all the static files within the public folder
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+  //res.send('Hello Olha and Hello World!')
+  res.render('index'); // now we use res.render as it will render our view then send equivalent html to the client
+});
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
