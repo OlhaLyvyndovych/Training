2693f428c0d9838a308494a24472e062487e9b9b
Olha L <olha.lyvyndovych@gmail.com>
2018-12-13 11:52:16 +0200

    Update of bootcamp files

--- O.Lyvyndovych/index.js
+++ O.Lyvyndovych/index.js
@@ -8,10 +8,10 @@ form.addEventListener("submit", function(event){
 });
 
 $(document).ready(function(){
-    var date_input=$('input[name="date"]'); //our date input has the name "date"
+    var date_input=$('input[name="date"]');
     var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
     var options={
-      format: 'mm/dd/yyyy',
+      format: 'dd/mm/yyyy',
       container: container,
       todayHighlight: true,
       autoclose: true,
@@ -19,14 +19,50 @@ $(document).ready(function(){
     date_input.datepicker(options);
 });
 
-function validateName(){
-    var firstName = document.getElementById('#firstName');
-    var firstNameReg = /[^'\x22]+/;
-    var result = firstNameReg.test(firstName);
-    if (result == false){
-      alert('Please enter a valid name');
-      return false;
-    } else {
-    return true
-    };
+const patterns = {
+  names: /^[a-z]+$/i,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  password: /^[\d\w@-]{8,20}$/i,
+  address: /^[a-z0-9]+$/i
 };
+
+
+ function validateFirst(){
+    let firstName = document.getElementById('firstName').value;
+    let firstResult = patterns.names.test(firstName);
+    if (firstResult == false) alert('Please correct your first name');
+};
+
+
+
+  function validateLast(){
+     let lastName = document.getElementById('lastName').value;
+     let lastResult = patterns.names.test(lastName);
+     if (lastResult == false) alert('Please correct your last name');
+
+   };
+
+   function validateBirthday(){
+      let birthday = document.getElementById('date').value;
+      if (birthday == '') alert('Please fill in the field');
+
+    };
+
+    function validateCountry(){
+       let country = document.getElementById('country').value;
+       if (country == ' ') alert('Please choose a country');
+
+     };
+
+   function validateEmail(){
+      let email = document.getElementById('email').value;
+      let emailResult = patterns.email.test(email);
+      if (emailResult == false) alert('Please correct your email');
+
+    };
+
+    function validatePassword(){
+       let password = document.getElementById('password').value;
+       let passwordResult = patterns.password.test(password);
+       if (passwordResult == false) alert('Please correct your password');
+     };
